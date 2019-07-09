cd4c7423d268023ca471ca5bfb1257d27d8e67e8
camueljackson <camille.a.pelletier@gmail.com>
2019-07-09 02:19:46 -0400

    first commit; app structure and basic imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <app-customers></app-customers>
+  `,
 })
-export class AppComponent {
-  title = 'my-dream-app';
+export class AppComponent implements OnInit {
+  title: string;
+  constructor() {}
+
+  ngOnInit() {
+    // We call a service that gets us the data
+    this.title = 'Hello World';
+  }
 }
